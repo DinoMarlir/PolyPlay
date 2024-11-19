853cc03ace7b7bf8b2085838bad87baf18f3ca2e
Marlon <72864666+DinoMarlir@users.noreply.github.com>
2024-11-19 12:22:05 +0100

    cleaned up kotlin tests by removing DisplayName annontation and use function names with `` (kotlin)

--- core/src/test/kotlin/com/github/dinomarlir/polyplay/core/tests/KotlinPolyPlayTests.kt
+++ core/src/test/kotlin/com/github/dinomarlir/polyplay/core/tests/KotlinPolyPlayTests.kt
@@ -3,30 +3,26 @@ package com.github.dinomarlir.polyplay.core.tests
 import com.github.dinomarlir.polyplay.core.kt.PolyPlay
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertNotNull
-import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
 
 class KotlinPolyPlayTests {
 
     @Test
-    @DisplayName("Test if PolyPlay instance is not null")
     @Order(1)
-    fun testPolyPlayInstance() = assertNotNull(PolyPlay)
+    fun `Test if PolyPlay instance is not null`() = assertNotNull(PolyPlay)
 
 
     @Test
-    @DisplayName("Test if you can register and get an instance")
     @Order(2)
-    fun testRegisterAndGetInstanceOf() {
+    fun `Test if you can register and get an instance`() {
         PolyPlay.setInstanceOf(String::class.java, "Hello, World!")
         Assertions.assertEquals("Hello, World!", PolyPlay.getInstanceOf(String::class.java).get())
     }
 
     @Test
-    @DisplayName("Test if you can remove an instance")
     @Order(3)
-    fun testRemoveInstanceOf() {
+    fun `Test if you can remove an instance`() {
         PolyPlay.setInstanceOf(String::class.java, "Hello, World!")
         PolyPlay.removeInstanceOf(String::class.java)
         Assertions.assertEquals(true, PolyPlay.getInstanceOf(String::class.java).isEmpty)
